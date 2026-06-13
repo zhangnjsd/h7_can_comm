92d7352b3f394e2874269c5a08f3a42427a78f8f
zhangnjsd <zhangnjsd@qq.com>
2026-06-13 22:30:29 +0800

    remote

--- Application/can_comm_task_fun.c
+++ Application/can_comm_task_fun.c
@@ -5,19 +5,6 @@ void can_comm_task_fun(void *argument) {
   // ? FD CAN Setup should be done in main.c, and the handle should be declared
   // in main.h.
   uint32_t tick;
-
-  // Send any messages.
-  uint8_t msg[] = {1, 0, 0, 0};
-
-  FDCAN_TxHeaderTypeDef tx_header = {.Identifier = REMOTE_ID,
-                                     .IdType = FDCAN_STANDARD_ID,
-                                     .TxFrameType = FDCAN_DATA_FRAME,
-                                     .DataLength = FDCAN_DLC_BYTES_4};
-
-  // Transmit the message.
-  if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &tx_header, msg) != HAL_OK) {
-    Error_Handler();
-  }
   /* Infinite loop */
   for (;;) {
     tick = osKernelGetTickCount();
@@ -37,10 +24,20 @@ void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan,
     if (HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &rx_header, rx_data) !=
         HAL_OK) {
       Error_Handler();
-    }
+    } else {
 
-    for (int i = 0; i < 4; i++) {
-      recv_buf[i] = rx_data[i];
+      // Send any messages.
+      rx_data[0] += 1;
+
+      FDCAN_TxHeaderTypeDef tx_header = {.Identifier = LOCAL_ID,
+                                         .IdType = FDCAN_STANDARD_ID,
+                                         .TxFrameType = FDCAN_DATA_FRAME,
+                                         .DataLength = FDCAN_DLC_BYTES_4};
+
+      // Transmit the message.
+      if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &tx_header, rx_data) != HAL_OK) {
+        Error_Handler();
+      }
     }
 
     // Process the received message (for example, print it or toggle an LED).
